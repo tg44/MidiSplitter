a01b8d55c5e58c14d02a665580f6409a60b46a53
Torcsvari Gergo <torcsvari.gergo@smartfront.hu>
2014-02-22 09:46:19 +0100

    dir spliting (recursive not implemented yet) code starting to accept switches the chord splitting implementation started
    !!!Not tested commit, its build but it coud be fail on run!!!

--- src/midisplitter/MidiSplitter.java
+++ src/midisplitter/MidiSplitter.java
@@ -3,10 +3,11 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package midisplitter;
 
 import java.io.File;
+import java.util.HashMap;
+import java.util.Map.Entry;
 import javax.sound.midi.MidiSystem;
 import javax.sound.midi.Sequence;
 
@@ -20,24 +21,61 @@ public class MidiSplitter {
      * @param args the command line arguments
      */
     public static void main(String[] args) throws Exception {
-        if(args.length<2)
-            System.out.println("The correct usage is: MidiSplitter input.mid output.mid");
+        if (args.length < 2) {
+            System.out.println("Correct usage:");
+            System.out.println("    MidiSplitter input.mid output.mid");
+            System.out.println("    MidiSplitter inputdir -po postfix [-r]");
+            System.out.println("    MidiSplitter inputdir -pr prefix [-r]");
+        }
+        HashMap<String, String> runon = new HashMap<String, String>();
         //Tester t=new Tester();
         //t.test("d:\\pirates.mid");
-        Splitter sp=new Splitter();
-        Sequence sequence = MidiSystem.getSequence(new File(args[0]));
-        sp.splitit(sequence);
-        ReOrganizer reo=new ReOrganizer(sp);
-        int chnum=reo.firstComeFirstServe();
-        if(chnum>16)
-            throw new Exception("too much chennel: "+chnum);
-        Sequence oseq=new Sequence(sequence.getDivisionType(), sequence.getResolution());
-        OutputWriter opw=new OutputWriter();
-        opw.write(oseq, reo);
-        int[] allowedTypes = MidiSystem.getMidiFileTypes(oseq);
-        MidiSystem.write(oseq,allowedTypes[0],new File(args[1]));
-        //System.out.println("--------------------------------");
-        //t.test("d:\\pirates_out.mid");
+
+        //one file
+        if (args[0].endsWith(".mid")) {
+            runon.put(args[0], args[1]);
+        }
+        //dir
+        if (args.length > 2) {
+            File homeDir = new File(args[0]);
+            for (File f : homeDir.listFiles()) {
+                if (f.getAbsolutePath().endsWith(".mid")) {
+                    if (args[1].contains("-po")) {
+                        String outFName = f.getAbsolutePath();
+                        outFName = outFName.replace(".mid", args[2] + ".mid");
+                        runon.put(f.getAbsolutePath(), outFName);
+                    }
+                    if (args[1].contains("-pr")) {
+                        String outFName = f.getParentFile().getAbsolutePath();
+                        outFName += args[2]+f.getName();
+                        runon.put(f.getAbsolutePath(), outFName);
+                        
+                    }
+                }
+            }
+            //recursive
+            if (args.length == 4 && args[3].equals("-r")) {
+                System.out.println("recursive wlak not implemented yet");
+            }
+        }
+        for (Entry<String, String> entry : runon.entrySet()) {
+            Splitter sp = new Splitter();
+            Sequence sequence = MidiSystem.getSequence(new File(entry.getKey()));
+            sp.splitit(sequence);
+            ReOrganizer reo = new ReOrganizer(sp);
+
+            int chnum = reo.firstComeFirstServe();
+            if (chnum > 16) {
+                throw new Exception("too much chennel: " + chnum);
+            }
+            Sequence oseq = new Sequence(sequence.getDivisionType(), sequence.getResolution());
+            OutputWriter opw = new OutputWriter();
+            opw.write(oseq, reo);
+            int[] allowedTypes = MidiSystem.getMidiFileTypes(oseq);
+            MidiSystem.write(oseq, allowedTypes[0], new File(entry.getValue()));
+            //System.out.println("--------------------------------");
+            //t.test("d:\\pirates_out.mid");
+        }
     }
-    
+
 }
--- src/midisplitter/ReOrganizer.java
+++ src/midisplitter/ReOrganizer.java
@@ -21,6 +21,20 @@ public class ReOrganizer {
     Boolean keeptracks = true;
     Boolean keepchannels = true;
 
+    public void setSplittotracks(Boolean splittotracks) {
+        this.splittotracks = splittotracks;
+    }
+
+    public void setKeeptracks(Boolean keeptracks) {
+        this.keeptracks = keeptracks;
+    }
+
+    public void setKeepchannels(Boolean keepchannels) {
+        this.keepchannels = keepchannels;
+    }
+
+    
+    
     public ReOrganizer(Splitter sp) {
         if (sp.getSeparatedNotes() == null) {
             throw new NullPointerException();
@@ -78,6 +92,62 @@ public class ReOrganizer {
         return lists.size();
     }
 
+    int chordIsIsolatedSame() {
+        ArrayList<ArrayList<MidiNote>> lists = new ArrayList<ArrayList<MidiNote>>();
+        for (Entry<String, HashMap<String, ArrayList<MidiNote>>> entry2 : sp.getSeparatedNotes().entrySet()) {
+            ArrayList<ArrayList<MidiNote>> trackLists = new ArrayList<ArrayList<MidiNote>>();
+            if (!keeptracks) {
+                trackLists = lists;
+            }
+            for (Entry<String, ArrayList<MidiNote>> entry : entry2.getValue().entrySet()) {
+                ArrayList<ArrayList<MidiNote>> channelLists = new ArrayList<ArrayList<MidiNote>>();
+                ArrayList<MidiNote> processList = entry.getValue();
+                if (!keepchannels) {
+                    channelLists = trackLists;
+                }
+                for (MidiNote mn : processList) {
+                    Boolean found = false;
+                    for (int i=0;i<channelLists.size();i++) {
+                        ArrayList<MidiNote> list=channelLists.get(i);
+                        if (!onHit(list, mn)) {
+                            list.add(mn);
+                            found = true;
+                            break;
+                        } else {
+                            MidiNote tmp1=onSamePos(list, mn);
+                            if(tmp1!=null){
+                                //TODO
+                            }
+                        }
+                    }
+                    if (!found) {
+                        ArrayList<MidiNote> newlist = new ArrayList<MidiNote>();
+                        newlist.add(mn);
+                        channelLists.add(newlist);
+                    }
+                }
+                if (keepchannels) {
+                    trackLists.addAll(channelLists);
+                }
+            }
+            if (keeptracks) {
+                lists.addAll(trackLists);
+            }
+
+        }
+        if (splittotracks) {
+            int tracks = 1;
+            for (ArrayList<MidiNote> chanellist : lists) {
+                organizedNotes.put("t" + tracks, new HashMap<String, ArrayList<MidiNote>>());
+                organizedNotes.get("t" + tracks).put("ch" + (tracks - 1), chanellist);
+                tracks++;
+            }
+        }
+
+        return lists.size();
+    }
+
+    
     public static Boolean onHit(ArrayList<MidiNote> noteList, MidiNote note) {
         for (MidiNote mn : noteList) {
             // mnstart--------------------mnend
@@ -100,6 +170,14 @@ public class ReOrganizer {
         return false;
     }
 
+    public static MidiNote onSamePos(ArrayList<MidiNote> noteList, MidiNote note) {
+        for (MidiNote mn : noteList) {
+            if(mn.startTick()==note.startTick() && mn.endTick()==note.endTick()){
+                return mn;
+            }
+        }
+        return null;
+    }
     public HashMap<String, HashMap<String, ArrayList<MidiNote>>> getOrganizedNotes() {
         return organizedNotes;
     }
